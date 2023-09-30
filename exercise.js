9dbc2c4c53101165b6629d5fccc1f2f3c8f77c47
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-09-30 16:13:21 +0200

    solution

--- exercise.js
+++ exercise.js
@@ -3,8 +3,7 @@ let num2 = 20;
 console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
 // Variable swapping
-let temp = num2;
-num2 = num1;
-num1 = temp;
+let arr = [num1, num2];
+[num2, num1] = arr;
 
 console.log('After swap: ', num1, num2); // After swap: 20 10
